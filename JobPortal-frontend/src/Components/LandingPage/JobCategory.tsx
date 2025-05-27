1789b438de8059ea4ed8b462244814d452135ff2
Risb7970 <gtm.rishabh2005@gmail.com>
2025-05-27 21:49:00 +0530

    changes in JobCategory

--- JobPortal-frontend/src/Components/LandingPage/JobCategory.tsx
+++ JobPortal-frontend/src/Components/LandingPage/JobCategory.tsx
@@ -15,7 +15,7 @@ const JobCategory = () => {
              previousControlIcon={<IconArrowLeft className="h-8 w-8" />}
             >
                 {
-                    jobCategory.map((category, index) => <Carousel.Slide>
+                    jobCategory.map((category, index) => <Carousel.Slide key={index}>
                         <div className="flex flex-col items-center w-64  sm-mx:w-56 xs-mx:w-48 gap-2 border border-bright-sun-400 p-5 rounded-xl hover
                         :cursor-pointer hover:shadow-[0_0_5px_2px_black] !shadow-bright-sun-300 my-5 transition duration-300 ease-in-out">
                             <div className="p-2 bg-bright-sun-300 rounded-full">
